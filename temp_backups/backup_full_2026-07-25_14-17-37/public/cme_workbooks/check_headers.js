7e9cddbe322383f9156c1d837ce3521542fe4a60
benlovettbl-blip <benlovett.bl@gmail.com>
2026-07-29 01:15:28 +0100

    Update gitignore to exclude backups

--- temp_backups/backup_full_2026-07-25_14-17-37/public/cme_workbooks/check_headers.js
+++
@@ -1,8 +0,0 @@
-fetch('https://edexcelgcsehistorycme.netlify.app')
-  .then(res => {
-    console.log('Status:', res.status);
-    for (let [key, val] of res.headers.entries()) {
-      console.log(`${key}: ${val}`);
-    }
-  })
-  .catch(err => console.error(err));
