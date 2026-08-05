4138e26a610303073935a7f9f1e4427319babcb1
benlovettbl-blip <benlovett.bl@gmail.com>
2026-08-06 00:17:41 +0100

    Auto-backup checkpoint before adding Lesson 5

---
+++ search_images5.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+async function search(title) {
+    const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${encodeURIComponent(title)}&prop=pageimages&pithumbsize=500&format=json`;
+    const res = await fetch(searchUrl);
+    const json = await res.json();
+    const pages = json.query?.pages;
+    if (!pages) { console.log(title, 'No results'); return; }
+    const pageWithThumb = Object.values(pages).filter(p => p.thumbnail).sort((a, b) => (a.index || 99) - (b.index || 99))[0];
+    if (pageWithThumb) {
+        console.log(title, ':', pageWithThumb.thumbnail.source);
+    } else {
+        console.log(title, 'No thumbnail');
+    }
+}
+(async () => {
+    await search("Triangular trade map");
+    await search("Slave ship Brookes diagram");
+    await search("Olaudah Equiano portrait 1789");
+    await search("Jamaica Maroon map 18th century");
+})();
